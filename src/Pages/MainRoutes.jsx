ca70bbe1ee68e1e2e7bf00a8583082157d803101
Sarika Singh <97461783+sarikasingh30@users.noreply.github.com>
2022-07-21 01:36:20 +0530

    commit

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -2,14 +2,17 @@
 
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
+import CasualSlippers from '../Components/Categories/CasualSlippers'
 import BestSeller from './BestSeller'
 import HomePage from './HomePage'
+import Login from "./Login"
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/bestseller" element={<BestSeller/>}/>
+        <Route path="/product-category/casual-slippers" element={<CasualSlippers/>}/>
     </Routes>
   );
 };
