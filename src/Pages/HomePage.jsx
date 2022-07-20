56e00551dfea20423233d1faa879d343c62b75b3
Harish8801 <92293513+Harish8801@users.noreply.github.com>
2022-07-20 23:57:29 +0530

    today code

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import Footer from "../Components/Footer";
-import Navbar from "../Components/Navbar";
+import React from 'react'
+import Footer from '../Components/Footer'
+import Navbar from '../Components/Navbar'
 
 const HomePage = () => {
   return (
     <div>
-      hi this is jai
-      <Navbar />
-      <Footer />
+      Home
+      <Navbar/>
+      <Footer/>
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
