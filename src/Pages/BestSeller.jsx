fee97c29d5a19e4151536b54925ed78d60d37a6b
Sarika Singh <97461783+sarikasingh30@users.noreply.github.com>
2022-07-20 10:52:41 +0530

    created BestSeller Page

---
+++ src/Pages/BestSeller.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BestSeller = () => {
+  return (
+    <div>
+      Best Seller
+    </div>
+  )
+}
+
+export default BestSeller
--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
+import BestSeller from './BestSeller'
 import HomePage from './HomePage'
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<HomePage/>}/>
-        {/* <Route path="......" element={.........}/> */}
+        <Route path="/bestseller" element={<BestSeller/>}/>
     </Routes>
   )
 }
