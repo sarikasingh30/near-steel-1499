2fe89fd003e36ae6233e7b7f0a46dbffd12837a0
jitesh-95 <1995sirohis@gmail.com>
2022-07-20 20:27:42 +0530

    login page done

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import {Routes,Route} from "react-router-dom"
-import HomePage from './HomePage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import Login from "./Login";
 const MainRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        {/* <Route path="......" element={.........}/> */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
