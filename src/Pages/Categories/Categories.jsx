3cea2a66bbd4c78641c7310a1d857943f4a36f5e
Indherjith <buvanesifet26@gmail.com>
2022-07-20 11:29:45 +0530

    Categories Section created
    Is in Progress

---
+++ src/Pages/Categories/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CasualSlippers from "./CasualSlippers"
+
+const Categories = () => {
+  return (
+    <div>
+      <CasualSlippers/>
+    </div>
+  )
+}
+
+export default Categories
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
+import Categories from './Categories/Categories'
 
 const HomePage = () => {
   return (
     <div>
       Home
       <Navbar/>
+      <Categories/>
       <Footer/>
     </div>
   )
