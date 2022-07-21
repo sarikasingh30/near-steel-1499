f5f54b27b8709653bf0f84baa1a4b9b43e4d25ea
jitesh-95 <1995sirohis@gmail.com>
2022-07-21 22:08:36 +0530

    dashboard page done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,18 +1,15 @@
-
 import React from "react";
 import CasualSlippers from "../Components/Categories/CasualSlippers";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import HomeTopSliding from "./HomeTopSliding";
 
-
-
 const HomePage = () => {
   return (
     <div>
       <Navbar />
-      {/* <HomeTopSliding /> */}
-      <CasualSlippers/>
+      <HomeTopSliding />
+      {/* <CasualSlippers/> */}
       <Footer />
     </div>
   );
